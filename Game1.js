5ba49ba07b3a7863aa49da12850b1a2e8727712f
U-CORP\jbell <jbell@WIN-5P2RVK5H557.corp.amazonworkspaces.com>
2017-03-20 18:38:18 +0000

    first commit

---
+++ Game1.js
@@ -0,0 +1,25 @@
+// var p1= document.querySelector("p");
+// p1.addEventListener("mouseover",translate);
+
+// function translate(){
+// 	p1.textContent="Hola";
+// }
+// p1.addEventListener("mouseout",translate1);
+// function translate1(){
+// 	p1.textContent="Hello"
+// }
+
+// var lis = document.getElementsByTagName("li");
+// for(var i = 0; i< lis.length; i++){
+// 	lis[i].addEventListener("click", function(){
+// 		console.log("you clicked an li");
+// 	});
+// }
+// var body=document.querySelector("body")
+// body.addEventListener("click", function({
+	
+// })
+var player1=document.getElementsById("p1");
+player1.addeventListener("click",function(){
+	pl[0]++;
+}
