330a08e1abf63029066a586ffb3eec7c2f8e2080
U-CORP\jbell <jbell@WIN-5P2RVK5H557.corp.amazonworkspaces.com>
2017-03-22 15:47:57 +0000

    almost done

--- Game1.js
+++ Game1.js
@@ -19,7 +19,59 @@
 // body.addEventListener("click", function({
 	
 // })
-var player1=document.getElementsById("p1");
-player1.addeventListener("click",function(){
-	pl[0]++;
+
+var player2Score=0;
+
+var player1Score=0
+var inc=document.getElementById("inc");
+
+var pl1=document.querySelector("#pl1");
+var btn= document.querySelector("#p1");
+btn.addEventListener("click", function(){
+	if(player1Score < inc.value){
+		player1Score++;
+	}
+	if(player2Score===inc.value){
+		player1Score;
+	}
+	
+	pl1.textContent=player1Score;
+});
+
+
+var pl2=document.querySelector("#pl2");
+var btn2= document.querySelector("#p2");
+btn2.addEventListener("click", function(){
+	if(player2Score < inc.value){
+		player2Score++;
+	}
+	if(player1Score===inc.value){
+		player2Score;
+	}
+	pl2.textContent=player2Score;
+});
+
+var ply=document.getElementById("ply");
+inc.addEventListener("change",function(){
+	ply.textContent=inc.value
+});
+
+
+
+
+
+
+
+
+
+//var player1=document.getElementById("p1");
+// player1.addeventListener("click",score();
+// function score(){
+// 	pl1.textContent
+
+function volume(l,w,h){
+	return l+w-h;
 }
+var length=12;
+var width=8;
+var height=3;
